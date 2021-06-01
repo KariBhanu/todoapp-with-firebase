814e8c1632dbefb6b76179a5e11b88de6f540c3c
KariBhanu <bhanu.kari@quantiphi.com>
2021-06-01 12:03:02 +0530

    initail commit

---
+++ Desktop/Quantiphi/todo-app-firebase/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDyr4ook2yFsAo1eWKMwHpcViAa0oxynTw",
+    authDomain: "todo-with-firebase-1336a.firebaseapp.com",
+    projectId: "todo-with-firebase-1336a",
+    storageBucket: "todo-with-firebase-1336a.appspot.com",
+    messagingSenderId: "110023773046",
+    appId: "1:110023773046:web:f5d5bd90107cca4e930857",
+    measurementId: "G-FG32NVDS13"
+  }
+};
---
+++ Desktop/Quantiphi/todo-app-firebase/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDyr4ook2yFsAo1eWKMwHpcViAa0oxynTw",
+    authDomain: "todo-with-firebase-1336a.firebaseapp.com",
+    projectId: "todo-with-firebase-1336a",
+    storageBucket: "todo-with-firebase-1336a.appspot.com",
+    messagingSenderId: "110023773046",
+    appId: "1:110023773046:web:f5d5bd90107cca4e930857",
+    measurementId: "G-FG32NVDS13"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
